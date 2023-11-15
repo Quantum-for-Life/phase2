7d140358b57f65c82b5b01b553cca5077fd3e403
⧉⧉⧉ <marek.miller@protonmail.com>
2023-11-15 16:57:00 +0100

    Move define's to the header

--- src/linen.h
+++ src/linen.h
@@ -3,6 +3,11 @@
 
 #include "circ.h"
 
+#define LINEN_NAME "linen"
+#define LINEN_DEFAULT_NUM_MEA_QB 4
+#define LINEN_DEFAULT_NUM_SYS_QB 8
+#define LINEN_DEFAULT_NUM_ANC_QB 4
+
 circuit linen_circuit_factory(void *data);
 
 #endif //PHASE2_LINEN_H
