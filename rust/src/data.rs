8c782ea0e3cf0f177b34bdfec51afa8e38cbd7ec
Marek Miller <mlm@math.ku.dk>
2023-11-24 11:50:41 +0100

    Add PauliHamil data parser to test suite

--- rust/src/data.rs
+++ rust/src/data.rs
@@ -1,19 +1,26 @@
 use std::{
     ffi::CString,
+    mem::MaybeUninit,
     path::Path,
+    ptr::slice_from_raw_parts,
 };
+
+use crate::data::ffi::data_result::DATA_OK;
 
 mod ffi;
 
 #[derive(Debug)]
-enum Error {
+pub enum Error {
     FileOpen,
+    FileRead,
 }
 
-struct DataHandle(ffi::dataid_t);
+pub struct Empty<T>(T);
+
+pub struct DataHandle(ffi::dataid_t);
 
 impl DataHandle {
-    fn open(path: &Path) -> Result<Self, Error> {
+    pub fn open(path: &Path) -> Result<Self, Error> {
         let path_str = path.to_str().ok_or(Error::FileOpen)?;
         let filename = CString::new(path_str).map_err(|_| Error::FileOpen)?;
         let file_id = unsafe { ffi::data_file_open(filename.as_ptr()) };
@@ -33,19 +40,103 @@ impl Drop for DataHandle {
     }
 }
 
+#[derive(Debug)]
+pub struct PauliHamil(ffi::data_pauli_hamil);
+
+impl PauliHamil {
+    pub fn new() -> Empty<Self> {
+        let mut dat_uninit = MaybeUninit::uninit();
+        let dat = unsafe {
+            ffi::data_pauli_hamil_init(dat_uninit.as_mut_ptr());
+            dat_uninit.assume_init()
+        };
+        Empty(Self(dat))
+    }
+
+    pub fn num_qubits(&self) -> usize {
+        self.0.num_qubits
+    }
+
+    pub fn num_terms(&self) -> usize {
+        self.0.num_terms
+    }
+
+    pub fn coeffs(&self) -> &[f64] {
+        let slice_ptr = slice_from_raw_parts(self.0.coeffs, self.0.num_terms);
+        unsafe { &*slice_ptr }
+    }
+
+    pub fn paulis(&self) -> &[u8] {
+        let slice_ptr = slice_from_raw_parts(
+            self.0.paulis,
+            self.0.num_terms * self.0.num_qubits,
+        );
+        unsafe { &*slice_ptr }
+    }
+}
+
+impl Empty<PauliHamil> {
+    pub fn read(
+        mut self,
+        handle: &mut DataHandle,
+    ) -> Result<PauliHamil, Error> {
+        let res = unsafe {
+            ffi::data_pauli_hamil_read(&mut self.0 .0 as *mut _, handle.0)
+        };
+        (res == ffi::data_result::DATA_OK)
+            .then_some(self.0)
+            .ok_or(Error::FileRead)
+    }
+}
+
+impl Drop for PauliHamil {
+    fn drop(&mut self) {
+        unsafe { ffi::data_pauli_hamil_destroy(&mut self.0 as *mut _) };
+    }
+}
+
 #[cfg(test)]
 mod tests {
-    use std::path::{
-        Path,
-        PathBuf,
+    use std::{
+        iter::zip,
+        path::PathBuf,
     };
 
     use super::*;
+
+    const MARGIN: f64 = 1e-4;
 
     #[test]
     fn data_file_open() {
         let data_dir = PathBuf::from("./dat");
         let filename = data_dir.join("./simul_H2_2.h5");
         let handle = DataHandle::open(&filename).unwrap();
+    }
+
+    #[test]
+    fn data_pauli_hamil_read() {
+        let data_dir = PathBuf::from("./dat");
+        let filename = data_dir.join("./simul_H2_2.h5");
+        let mut handle = DataHandle::open(&filename).unwrap();
+
+        let data = PauliHamil::new().read(&mut handle).unwrap();
+        assert_eq!(data.num_qubits(), 4);
+        assert_eq!(data.num_terms(), 15);
+
+        let expected_coeffs = [
+            -1.16395, 0.298454, 0.00407158, 0.298454, 0.00407158, 0.0749901,
+            0.163535, 0.0857172, 0.0107271, 0.0107271, 0.0107271, 0.0107271,
+            0.0857172, 0.073949, 0.0749901,
+        ];
+        for (coeff, exp_coeff) in zip(data.coeffs(), expected_coeffs) {
+            assert!(f64::abs(coeff - exp_coeff) < MARGIN);
+        }
+
+        let expected_paulis = [
+            0, 0, 0, 0, 3, 0, 0, 0, 0, 3, 0, 0, 0, 0, 3, 0, 0, 0, 0, 3, 3, 3,
+            0, 0, 3, 0, 3, 0, 3, 0, 0, 3, 2, 2, 2, 2, 2, 2, 1, 1, 1, 1, 2, 2,
+            1, 1, 1, 1, 0, 3, 3, 0, 0, 3, 0, 3, 0, 0, 3, 3,
+        ];
+        assert_eq!(data.paulis(), &expected_paulis);
     }
 }
--- rust/src/data/ffi.rs
+++ rust/src/data/ffi.rs
@@ -3,7 +3,12 @@
 #![allow(dead_code)]
 #![allow(clippy::upper_case_acronyms)]
 
-use libc::c_char;
+use std::ffi::{
+    c_char,
+    c_double,
+    c_int,
+    c_uchar,
+};
 
 pub(crate) const DATA_INVALID_OBJID: i64 = -1;
 
@@ -21,33 +26,24 @@ extern "C" {
     pub(crate) fn data_file_close(file_id: dataid_t);
 }
 
-// #define DATA_INVALID_OBJID  (-1)
-//
-// typedef int64_t dataid_t; // This is the same as HDF5's hid_t
-//
-// dataid_t data_open_file(const char* filename);
-//
-// int data_close_file(dataid_t);
-//
-//
-// #define DATA_PAULI_HAMIL "pauli_hamil"
-// #define DATA_PAULI_HAMIL_COEFFS "coeffs"
-// #define DATA_PAULI_HAMIL_PAULIS "paulis"
-//
-// struct data_pauli_hamil {
-//     size_t num_qubits;
-//     size_t num_terms;
-//     double* coeffs;
-//     unsigned char* paulis;
-// };
-//
-// void data_pauli_hamil_init(struct data_pauli_hamil*);
-//
-// void data_pauli_hamil_destroy(struct data_pauli_hamil*);
-//
-// int data_pauli_hamil_read(struct data_pauli_hamil*, dataid_t);
-//
-//
+#[derive(Debug, PartialEq)]
+#[repr(C)]
+pub(crate) struct data_pauli_hamil {
+    pub(crate) num_qubits: usize,
+    pub(crate) num_terms:  usize,
+    pub(crate) coeffs:     *mut c_double,
+    pub(crate) paulis:     *mut c_uchar,
+}
+
+extern "C" {
+    pub(crate) fn data_pauli_hamil_init(dat: *mut data_pauli_hamil);
+    pub(crate) fn data_pauli_hamil_destroy(dat: *mut data_pauli_hamil);
+    pub(crate) fn data_pauli_hamil_read(
+        dat: *mut data_pauli_hamil,
+        file_id: dataid_t,
+    ) -> data_result;
+}
+
 // #define DATA_TIME_SERIES "time_series"
 // #define DATA_TIME_SERIES_TIMES "times"
 // #define DATA_TIME_SERIES_VALUES "values"
