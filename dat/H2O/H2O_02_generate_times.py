3a5d7cd11acb27900fa40539c73c77c283dba0e3
Marek Miller <mlm@math.ku.dk>
2023-11-30 11:04:40 +0100

    Copy and rename scripts for presentation

---
+++ dat/H2O/H2O_02_generate_times.py
@@ -0,0 +1,41 @@
+#!/bin/env/python
+
+import argparse
+
+import h5py
+import numpy as np
+
+
+def parse_arguments():
+    parser = argparse.ArgumentParser(
+        prog="jw_map",
+        description="Parse FCIDUMP to JW mapping",
+        epilog="Quantum-for-Life",
+    )
+
+    parser.add_argument("-o", "--output", type=str, help="Output file: .h5")
+    parser.add_argument("-v", "--verbose", action="store_true")
+    return parser.parse_args()
+
+
+STEPS = 32
+TIMES = [int(x * 3.7) for x in range(0, STEPS)]
+VALUES = np.ndarray((STEPS, 2), dtype="d")
+
+for i in range(0, STEPS):
+    VALUES[i, 0] = np.nan
+    VALUES[i, 1] = np.nan
+
+
+def h5_output(outfile: str):
+    with h5py.File(outfile, "a") as f:
+        grp = f.create_group("time_series")
+        grp.create_dataset("times", (STEPS,), dtype="d")[...] = TIMES
+        grp.create_dataset("values", (STEPS, 2), dtype="d")[...] = VALUES
+
+
+if __name__ == "__main__":
+    args = parse_arguments()
+
+    if args.output:
+        h5_output(args.output)
---
+++ dat/H2O/H2O_03_analyze_results.py
@@ -0,0 +1,38 @@
+from math import tau
+
+import h5py
+import matplotlib.pyplot as plt
+import numpy as np
+import scipy
+
+with h5py.File("../python/tmp/simul.h5", "r") as f:
+    times = np.array(f["time_series/times"])
+    values = np.array([complex(z[0], z[1]) for z in f["time_series/values"]])
+    ph = f["pauli_hamil"]
+    norm = ph.attrs["normalization"]
+    offset = ph.attrs["offset"]
+
+print(f"{norm=}")
+size = len(times)
+ts = [(times[i], values[i]) for i in range(size)]
+ts.sort()
+times = [t for t, _ in ts]
+values = [v for _, v in ts]
+
+x = np.array(range(int(min(times)), int(max(times)) + 1))
+fft_size = len(x)
+y = np.interp(x, times, values)
+
+y_fft = np.fft.fft(y)
+y_fft_abs = [abs(yf) for yf in y_fft]
+
+plt.plot(y_fft_abs)
+plt.show()
+
+fft_freqs = np.fft.fftfreq(fft_size)
+peaks = scipy.signal.find_peaks(y_fft_abs, threshold=fft_size * 0.01)[0]
+peaks_freqs = [fft_freqs[p] / norm * tau for p in peaks]
+peaks_freqs.sort()
+
+print(peaks_freqs)
+print([p + offset for p in peaks_freqs])
