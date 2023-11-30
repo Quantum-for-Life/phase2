7c93f7a093504cec64707c1dcb656068f82ae87d
Marek Miller <mlm@math.ku.dk>
2023-11-30 08:49:14 +0100

    Move test macros to a separate header

--- test/circ/main.c
+++ test/circ/main.c
@@ -1,43 +1,11 @@
 #include <stdarg.h>
 #include <stdio.h>
 
-#define TEST_INIT(desc)                                                       \
-        const char* __test_desc = desc;                                       \
-        int __test_res = 0;
+#include "test.h"
 
-#define TEST_FAIL(...)                                                        \
-        fprintf(stderr, "FAIL: %s:%d, ", __FILE__, __LINE__);                 \
-        fprintf(stderr, __VA_ARGS__);                                         \
-        fprintf(stderr, "\n");                                                \
-        goto __error;
+TEST(main, void)
 
-#define TEST_END                                                              \
-        fprintf(stderr, "PASS: %s\n", __test_desc);                           \
-        goto __exit;                                                          \
-        __error:  __test_res = -1;                                            \
-        __exit:
+        TEST_ASSERT(0 == 1, "Not good enough")
 
-#define TEST_RES __test_res
-
-int test_case(int c) {
-        TEST_INIT("test case")
-
-        if (c < 0) {
-                TEST_FAIL("something's wrong")
-        }
-
-        TEST_END
-        /* Your cleanup code here */
-
-        return TEST_RES;
-}
-
-
-int main() {
-        int rc = 0;
-
-        rc |= test_case(0);
-        rc |= test_case(1);
-
-        return rc;
-}
+        TEST_FIN
+TEST_END
---
+++ test/test.h
@@ -0,0 +1,33 @@
+#ifndef TEST_H
+#define TEST_H
+
+#include <stdio.h>
+
+#define TEST(name, ...)	\
+int name(__VA_ARGS__) { \
+const char* __test_name = #name; \
+int __test_rc;
+
+#define TEST_ASSERT(cond, ...)	\
+if (!(cond)) { \
+fprintf(stderr, "FAIL: %s\n", __test_name); \
+fprintf(stderr, "-- %s:%d\n-- ",  __FILE__, __LINE__);       \
+fprintf(stderr, __VA_ARGS__);	\
+fprintf(stderr, "\n");	\
+goto __test_error;	\
+}
+
+#define TEST_FIN	\
+__test_rc = 0;	\
+goto __test_exit;	\
+__test_error:	\
+__test_rc = -1;	\
+__test_exit:
+
+#define TEST_END	\
+return	__test_rc; \
+}
+
+#define TEST_CASE(exp) TEST_ASSERT(exp == 0, "%s", #exp)
+
+#endif // TEST_H
