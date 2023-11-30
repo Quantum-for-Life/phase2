09e7a373a35d77cf5cd20843ca820fb70db9d66a
Marek Miller <mlm@math.ku.dk>
2023-11-30 13:33:16 +0100

    Update data analysis scripts

--- dat/H2O/H2O_02_generate_times.py
+++ dat/H2O/H2O_02_generate_times.py
@@ -1,41 +1,55 @@
 #!/bin/env/python
 
 import argparse
+import random
+import shutil
+from pathlib import Path
 
 import h5py
 import numpy as np
 
 
 def parse_arguments():
-    parser = argparse.ArgumentParser(
-        prog="jw_map",
-        description="Parse FCIDUMP to JW mapping",
-        epilog="Quantum-for-Life",
-    )
-
-    parser.add_argument("-o", "--output", type=str, help="Output file: .h5")
-    parser.add_argument("-v", "--verbose", action="store_true")
+    parser = argparse.ArgumentParser()
+    parser.add_argument("filename")
+    parser.add_argument("-n", "--out-files", type=int, default=1,
+                        help="Number of output files")
+    parser.add_argument("-s", "--time-steps", type=int, default=128,
+                        help="Number of time steps in each file")
+    parser.add_argument("--time-min", type=float, default=0.0)
+    parser.add_argument("--time-max", type=float, default=100.0)
     return parser.parse_args()
 
 
-STEPS = 32
-TIMES = [int(x * 3.7) for x in range(0, STEPS)]
-VALUES = np.ndarray((STEPS, 2), dtype="d")
-
-for i in range(0, STEPS):
-    VALUES[i, 0] = np.nan
-    VALUES[i, 1] = np.nan
-
-
-def h5_output(outfile: str):
-    with h5py.File(outfile, "a") as f:
+def write_h5file(filename, times):
+    size = len(times)
+    values = np.ndarray((size, 2), dtype="d")
+    for i in range(0, size):
+        values[i, 0] = np.nan
+        values[i, 1] = np.nan
+    with h5py.File(filename, "a") as f:
         grp = f.create_group("time_series")
-        grp.create_dataset("times", (STEPS,), dtype="d")[...] = TIMES
-        grp.create_dataset("values", (STEPS, 2), dtype="d")[...] = VALUES
+        grp.create_dataset("times", (size,), dtype="d")[...] = times
+        grp.create_dataset("values", (size, 2), dtype="d")[...] = values
 
 
 if __name__ == "__main__":
     args = parse_arguments()
 
-    if args.output:
-        h5_output(args.output)
+    num_files = args.out_files
+    time_steps = args.time_steps
+    time_min = args.time_min
+    time_max = args.time_max
+
+    times = set()
+    while len(times) < num_files * time_steps:
+        times.add(random.randint(time_min, time_max))
+    times = list(times)
+    random.shuffle(times)
+
+    h5file = args.filename
+    for fi in range(args.out_files):
+        filename = Path(h5file).stem + f"_{fi:03}.h5";
+        shutil.copy(h5file, filename)
+        time_slice = times[fi*time_steps:(fi+1)*time_steps]
+        write_h5file(filename, time_slice)
--- dat/H2O/H2O_03_analyze_results.py
+++ dat/H2O/H2O_03_analyze_results.py
@@ -1,38 +1,65 @@
 from math import tau
 
+import argparse
+
 import h5py
 import matplotlib.pyplot as plt
 import numpy as np
 import scipy
 
-with h5py.File("../python/tmp/simul.h5", "r") as f:
-    times = np.array(f["time_series/times"])
-    values = np.array([complex(z[0], z[1]) for z in f["time_series/values"]])
-    ph = f["pauli_hamil"]
-    norm = ph.attrs["normalization"]
-    offset = ph.attrs["offset"]
 
-print(f"{norm=}")
-size = len(times)
-ts = [(times[i], values[i]) for i in range(size)]
-ts.sort()
-times = [t for t, _ in ts]
-values = [v for _, v in ts]
+def parse_arguments():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("files", type=str, nargs='+')
+    return parser.parse_args()
 
-x = np.array(range(int(min(times)), int(max(times)) + 1))
-fft_size = len(x)
-y = np.interp(x, times, values)
 
-y_fft = np.fft.fft(y)
-y_fft_abs = [abs(yf) for yf in y_fft]
+if __name__ == "__main__":
 
-plt.plot(y_fft_abs)
-plt.show()
+    data_files = []
+    args = parse_arguments()
+    for filename in args.files:
+        data = {}
+        with h5py.File(filename, "r") as f:
+            data["times"] = np.array(f["time_series/times"])
+            data["values"] = np.array(
+                [complex(z[0], z[1]) for z in f["time_series/values"]])
+            ph = f["pauli_hamil"]
+            data["norm"] = ph.attrs["normalization"]
+            data["offset"] = ph.attrs["offset"]
+        data_files.append(data)
 
-fft_freqs = np.fft.fftfreq(fft_size)
-peaks = scipy.signal.find_peaks(y_fft_abs, threshold=fft_size * 0.01)[0]
-peaks_freqs = [fft_freqs[p] / norm * tau for p in peaks]
-peaks_freqs.sort()
+    norm_table = [data["norm"] for data in data_files]
+    offset_table = [data["offset"] for data in data_files]
+    assert all(norm == norm_table[0] for norm in norm_table)
+    assert all(offset == offset_table[0] for offset in offset_table)
+    norm = norm_table[0]
+    offset = offset_table[0]
 
-print(peaks_freqs)
-print([p + offset for p in peaks_freqs])
+    times = [t for data in data_files for t in data["times"]]
+    values = [v for data in data_files for v in data["values"]]
+
+    size = len(times)
+    assert len(values) == size
+
+    ts = [(times[i], values[i]) for i in range(size)]
+    ts.sort()
+    times = [t for t, _ in ts]
+    values = [v for _, v in ts]
+
+    x = np.array(range(int(min(times)), int(max(times)) + 1))
+    fft_size = len(x)
+    y = np.interp(x, times, values)
+
+    y_fft = np.fft.fft(y)
+    y_fft_abs = [abs(yf) for yf in y_fft]
+    plt.plot(y_fft_abs)
+    # plt.show()
+    plt.savefig("y_fft_abs.pdf")
+
+    fft_freqs = np.fft.fftfreq(fft_size)
+    peaks = scipy.signal.find_peaks(y_fft_abs, threshold=fft_size * 0.1)[0]
+    peaks_freqs = [fft_freqs[p] / norm * tau for p in peaks]
+    peaks_freqs.sort()
+
+    print([p + offset for p in peaks_freqs])
