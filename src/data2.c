a3a65c81dda9a32f0d7efe92029e2324b14894f5
⧉⧉⧉ <marek.miller@protonmail.com>
2024-03-27 12:55:18 +0100

    Change error codes in data2

--- src/data2.c
+++ src/data2.c
@@ -4,6 +4,17 @@
 
 #include "data2.h"
 
+enum {
+	DATA2_OK = 0,
+	DATA2_EDIM,
+	DATA2_EFILE,
+	DATA2_ENOMEM,
+	DATA2_ESTPREP,
+	DATA2_EMULDET,
+	DATA2_EHAMIL,
+	DATA2_ETROTT,
+};
+
 /* Group, dataset names */
 #define DATA2_STATE_PREP "state_prep"
 #define DATA2_STATE_PREP_MULTIDET "multidet"
@@ -15,10 +26,6 @@
 #define DATA2_PAULI_HAMIL_PAULIS "paulis"
 #define DATA2_PAULI_HAMIL_NORM "normalization"
 
-#define DATA2_TIME_SERIES "time_series"
-#define DATA2_TIME_SERIES_TIMES "times"
-#define DATA2_TIME_SERIES_VALUES "values"
-
 #define DATA2_TROTTER_STEPS "trotter_steps"
 #define DATA2_TROTTER_STEPS_TIME_FACTOR "time_factor"
 #define DATA2_TROTTER_STEPS_VALUES "values"
@@ -27,18 +34,16 @@
 data2_id
 data2_open(const char *filename)
 {
-	hid_t file_id, access_plist;
-
 	// init MPI environment
 	int initialized;
 	MPI_Initialized(&initialized);
 	if (!initialized) {
 		MPI_Init(NULL, NULL);
 	}
-	access_plist = H5Pcreate(H5P_FILE_ACCESS);
+	const hid_t access_plist = H5Pcreate(H5P_FILE_ACCESS);
 	H5Pset_fapl_mpio(access_plist, MPI_COMM_WORLD, MPI_INFO_NULL);
 
-	file_id = H5Fopen(filename, H5F_ACC_RDWR, access_plist);
+	const hid_t file_id = H5Fopen(filename, H5F_ACC_RDWR, access_plist);
 	if (file_id == H5I_INVALID_HID) {
 		return DATA2_INVALID_FID;
 	}
@@ -54,14 +59,14 @@ data2_close(const data2_id fid)
 
 /* --- State prep --- */
 static int
-state_prep_open(data2_id fid, hid_t *grpid)
+state_prep_open(const data2_id fid, hid_t *grpid)
 {
-	hid_t hid = H5Gopen2(fid, DATA2_STATE_PREP, H5P_DEFAULT);
+	const hid_t hid = H5Gopen2(fid, DATA2_STATE_PREP, H5P_DEFAULT);
 	if (hid == H5I_INVALID_HID)
-		return -1;
+		return -DATA2_ESTPREP;
 	*grpid = hid;
 
-	return 0;
+	return DATA2_OK;
 }
 
 static void
@@ -78,21 +83,22 @@ struct multidet_handle {
 };
 
 static int
-multidet_open(data2_id fid, struct multidet_handle *md)
+multidet_open(const data2_id fid, struct multidet_handle *md)
 {
-	hid_t sp_id, md_id;
+	hid_t sp_id;
 
 	if (state_prep_open(fid, &sp_id) < 0)
-		return -1;
-	md_id = H5Gopen2(sp_id, DATA2_STATE_PREP_MULTIDET, H5P_DEFAULT);
+		return -DATA2_ESTPREP;
+	const hid_t md_id =
+		H5Gopen2(sp_id, DATA2_STATE_PREP_MULTIDET, H5P_DEFAULT);
 	if (md_id == H5I_INVALID_HID) {
 		state_prep_close(sp_id);
-		return -1;
+		return -DATA2_EMULDET;
 	}
 	md->state_prep_grpid = sp_id;
 	md->multidet_grpid   = md_id;
 
-	return 0;
+	return DATA2_OK;
 }
 
 static void
@@ -105,28 +111,31 @@ multidet_close(struct multidet_handle md)
 int
 data2_multidet_getnums(data2_id fid, size_t *num_qubits, size_t *num_dets)
 {
+	int ret = DATA2_OK;
+
 	struct multidet_handle md;
 
-	if (multidet_open(fid, &md) < 0)
+	if (multidet_open(fid, &md) < 0) {
+		ret = -DATA2_EFILE;
 		goto err_open;
+	}
 
 	const hid_t dset_id = H5Dopen2(
 		md.multidet_grpid, DATA2_STATE_PREP_MULTIDET_DETS, H5P_DEFAULT);
-	if (dset_id == H5I_INVALID_HID)
+	if (dset_id == H5I_INVALID_HID) {
+		ret = -DATA2_EMULDET;
 		goto err_md_open;
+	}
 
 	const hid_t dsp_id = H5Dget_space(dset_id);
 	hsize_t	    dsp_dims[2];
-	if (H5Sget_simple_extent_dims(dsp_id, dsp_dims, NULL) != 2)
+	if (H5Sget_simple_extent_dims(dsp_id, dsp_dims, NULL) != 2) {
+		ret = -DATA2_EDIM;
 		goto err_dims;
+	}
 
 	*num_dets   = dsp_dims[0];
 	*num_qubits = dsp_dims[1];
-
-	H5Sclose(dsp_id);
-	H5Dclose(dset_id);
-	multidet_close(md);
-	return 0;
 
 err_dims:
 	H5Sclose(dsp_id);
@@ -134,36 +143,43 @@ err_dims:
 err_md_open:
 	multidet_close(md);
 err_open:
-	return -1;
+	return ret;
 }
 
 static int
 multidet_read_data(data2_id fid, double *coeffs, unsigned char *dets)
 {
+	int ret = DATA2_OK;
+
 	struct multidet_handle md;
-	if (multidet_open(fid, &md) < 0)
+	if (multidet_open(fid, &md) < 0) {
+		ret = -DATA2_EMULDET;
 		goto err_multidet_open;
+	}
 
 	const hid_t dset_coeffs_id = H5Dopen2(md.multidet_grpid,
 		DATA2_STATE_PREP_MULTIDET_COEFFS, H5P_DEFAULT);
-	if (dset_coeffs_id == H5I_INVALID_HID)
+	if (dset_coeffs_id == H5I_INVALID_HID) {
+		ret = DATA2_EMULDET;
 		goto err_coeffs_open;
+	}
 	if (H5Dread(dset_coeffs_id, H5T_NATIVE_DOUBLE, H5S_ALL, H5S_ALL,
-		    H5P_DEFAULT, coeffs) < 0)
+		    H5P_DEFAULT, coeffs) < 0) {
+		ret = -DATA2_EMULDET;
 		goto err_coeffs_read;
+	}
 
 	const hid_t dset_dets_id = H5Dopen2(
 		md.multidet_grpid, DATA2_STATE_PREP_MULTIDET_DETS, H5P_DEFAULT);
-	if (dset_dets_id == H5I_INVALID_HID)
+	if (dset_dets_id == H5I_INVALID_HID) {
+		ret = -DATA2_EMULDET;
 		goto err_dets_open;
+	}
 	if (H5Dread(dset_dets_id, H5T_NATIVE_UCHAR, H5S_ALL, H5S_ALL,
-		    H5P_DEFAULT, dets) < 0)
+		    H5P_DEFAULT, dets) < 0) {
+		ret = -DATA2_EMULDET;
 		goto err_dets_read;
-
-	H5Dclose(dset_dets_id);
-	H5Dclose(dset_coeffs_id);
-	multidet_close(md);
-	return 0;
+	}
 
 err_dets_read:
 	H5Dclose(dset_dets_id);
@@ -173,30 +189,39 @@ err_coeffs_read:
 err_coeffs_open:
 	multidet_close(md);
 err_multidet_open:
-	return -1;
+
+	return ret;
 }
 
 int
 data2_multidet_foreach(data2_id fid,
 	int (*op)(double coeff[2], uint64_t idx, void *), void *op_data)
 {
-	int    rc = 0;
+	int    ret, rc = 0;
 	size_t num_qubits, num_dets;
 
-	if (data2_multidet_getnums(fid, &num_qubits, &num_dets) < 0)
+	if (data2_multidet_getnums(fid, &num_qubits, &num_dets) < 0) {
+		ret = -DATA2_EMULDET;
 		goto err_getnums;
+	}
 
 	double *coeffs_buf = malloc(sizeof *coeffs_buf * 2 * num_dets);
-	if (!coeffs_buf)
+	if (coeffs_buf == NULL) {
+		ret = -DATA2_ENOMEM;
 		goto err_alloc_coeffs;
+	}
 	unsigned char *dets_buf =
 		malloc(sizeof *dets_buf * num_dets * num_qubits);
-	if (!dets_buf)
+	if (dets_buf == NULL) {
+		ret = -DATA2_ENOMEM;
 		goto err_alloc_dets;
+	}
 
 	/* Read the content of the data file */
-	if (multidet_read_data(fid, coeffs_buf, dets_buf) < 0)
+	if (multidet_read_data(fid, coeffs_buf, dets_buf) < 0) {
+		ret = -DATA2_EMULDET;
 		goto err_data_read;
+	}
 
 	for (size_t i = 0; i < num_dets; i++) {
 		uint64_t it_idx = 0;
@@ -213,10 +238,7 @@ data2_multidet_foreach(data2_id fid,
 		if (rc != 0)
 			break;
 	}
-
-	free(dets_buf);
-	free(coeffs_buf);
-	return rc;
+	ret = rc;
 
 err_data_read:
 	free(dets_buf);
@@ -224,7 +246,8 @@ err_alloc_dets:
 	free(coeffs_buf);
 err_alloc_coeffs:
 err_getnums:
-	return -1;
+
+	return ret;
 }
 
 static int
@@ -232,10 +255,11 @@ hamil_open(data2_id fid, hid_t *grpid)
 {
 	const hid_t hamil_id = H5Gopen2(fid, DATA2_PAULI_HAMIL, H5P_DEFAULT);
 	if (hamil_id == H5I_INVALID_HID)
-		return -1;
+		return -DATA2_EHAMIL;
+
 	*grpid = hamil_id;
 
-	return 0;
+	return DATA2_OK;
 }
 
 static void
@@ -247,27 +271,30 @@ hamil_close(hid_t grpid)
 int
 data2_hamil_getnums(data2_id fid, size_t *num_qubits, size_t *num_terms)
 {
+	int ret = DATA2_OK;
+
 	hid_t grpid;
-	if (hamil_open(fid, &grpid) < 0)
+	if (hamil_open(fid, &grpid) < 0) {
+		ret = -DATA2_EHAMIL;
 		goto err_open;
+	}
 
 	const hid_t dset_id =
 		H5Dopen2(grpid, DATA2_PAULI_HAMIL_PAULIS, H5P_DEFAULT);
-	if (dset_id == H5I_INVALID_HID)
+	if (dset_id == H5I_INVALID_HID) {
+		ret = -DATA2_EHAMIL;
 		goto err_read;
+	}
 
 	const hid_t dsp_id = H5Dget_space(dset_id);
 	hsize_t	    dsp_dims[2];
-	if (H5Sget_simple_extent_dims(dsp_id, dsp_dims, NULL) != 2)
+	if (H5Sget_simple_extent_dims(dsp_id, dsp_dims, NULL) != 2) {
+		ret = -DATA2_EDIM;
 		goto err_dims;
+	}
 
 	*num_terms  = dsp_dims[0];
 	*num_qubits = dsp_dims[1];
-
-	H5Sclose(dsp_id);
-	H5Dclose(dset_id);
-	hamil_close(grpid);
-	return 0;
 
 err_dims:
 	H5Sclose(dsp_id);
@@ -275,64 +302,75 @@ err_dims:
 err_read:
 	hamil_close(grpid);
 err_open:
-	return -1;
+
+	return ret;
 }
 
 int
 data2_hamil_getnorm(data2_id fid, double *norm)
 {
+	int ret = DATA2_OK;
+
 	hid_t grpid;
-	if (hamil_open(fid, &grpid) < 0)
+	if (hamil_open(fid, &grpid) < 0) {
+		ret = -DATA2_EHAMIL;
 		goto err_open;
+	}
 
 	const hid_t attr_norm_id =
 		H5Aopen(grpid, DATA2_PAULI_HAMIL_NORM, H5P_DEFAULT);
-	if (attr_norm_id == H5I_INVALID_HID)
+	if (attr_norm_id == H5I_INVALID_HID) {
+		ret = -DATA2_EHAMIL;
 		goto err_attr_open;
+	}
 	double n;
-	if (H5Aread(attr_norm_id, H5T_NATIVE_DOUBLE, &n) < 0)
+	if (H5Aread(attr_norm_id, H5T_NATIVE_DOUBLE, &n) < 0) {
+		ret = -DATA2_EHAMIL;
 		goto err_attr_read;
+	}
 	*norm = n;
-
-	H5Aclose(attr_norm_id);
-	hamil_close(grpid);
-	return 0;
 
 err_attr_read:
 	H5Aclose(attr_norm_id);
 err_attr_open:
 	hamil_close(grpid);
 err_open:
-	return -1;
+
+	return ret;
 }
 
 static int
 hamil_read_data(data2_id fid, double *coeffs, unsigned char *paulis)
 {
+	int ret = DATA2_OK;
+
 	hid_t grpid;
 	if (hamil_open(fid, &grpid) < 0)
 		goto err_hamil_open;
 
 	const hid_t dset_coeffs_id =
 		H5Dopen2(grpid, DATA2_PAULI_HAMIL_COEFFS, H5P_DEFAULT);
-	if (dset_coeffs_id == H5I_INVALID_HID)
+	if (dset_coeffs_id == H5I_INVALID_HID) {
+		ret = -DATA2_EHAMIL;
 		goto err_coeffs_open;
+	}
 	if (H5Dread(dset_coeffs_id, H5T_NATIVE_DOUBLE, H5S_ALL, H5S_ALL,
-		    H5P_DEFAULT, coeffs) < 0)
+		    H5P_DEFAULT, coeffs) < 0) {
+		ret = -DATA2_EHAMIL;
 		goto err_coeffs_read;
+	}
 
 	const hid_t dset_paulis_id =
 		H5Dopen2(grpid, DATA2_PAULI_HAMIL_PAULIS, H5P_DEFAULT);
-	if (dset_paulis_id == H5I_INVALID_HID)
+	if (dset_paulis_id == H5I_INVALID_HID) {
+		ret = -DATA2_EHAMIL;
 		goto err_paulis_open;
+	}
 	if (H5Dread(dset_paulis_id, H5T_NATIVE_UCHAR, H5S_ALL, H5S_ALL,
-		    H5P_DEFAULT, paulis) < 0)
+		    H5P_DEFAULT, paulis) < 0) {
+		ret = -DATA2_EHAMIL;
 		goto err_pauli_read;
-
-	H5Dclose(dset_paulis_id);
-	H5Dclose(dset_coeffs_id);
-	hamil_close(grpid);
-	return 0;
+	}
 
 err_pauli_read:
 	H5Dclose(dset_paulis_id);
@@ -342,31 +380,40 @@ err_coeffs_read:
 err_coeffs_open:
 	hamil_close(grpid);
 err_hamil_open:
-	return -1;
+
+	return ret;
 }
 
 int
-data2_hamil_foreach(
-	data2_id fid, int (*op)(double, unsigned char *, void *), void *op_data)
+data2_hamil_foreach(const data2_id fid,
+	int (*op)(double, unsigned char *, void *), void *op_data)
 {
-	int	       rc = 0;
-	unsigned char *paulis, *paustr;
-	double	      *coeffs;
-	size_t	       num_qubits, num_terms;
+	int    ret, rc = 0;
+	size_t num_qubits, num_terms;
 
-	if (data2_hamil_getnums(fid, &num_qubits, &num_terms) < 0)
+	if (data2_hamil_getnums(fid, &num_qubits, &num_terms) < 0) {
+		ret = -DATA2_EHAMIL;
 		goto err_getnums;
-	coeffs = malloc(sizeof *coeffs * num_terms);
-	if (!coeffs)
+	}
+	double *coeffs = malloc(sizeof *coeffs * num_terms);
+	if (coeffs == NULL) {
+		ret = -DATA2_EHAMIL;
 		goto err_coeffs_alloc;
-	paulis = malloc(sizeof *paulis * num_qubits * num_terms);
-	if (!paulis)
+	}
+	unsigned char *paulis = malloc(sizeof *paulis * num_qubits * num_terms);
+	if (paulis == NULL) {
+		ret = -DATA2_EHAMIL;
 		goto err_paulis_alloc;
-	if (hamil_read_data(fid, coeffs, paulis) < 0)
+	}
+	if (hamil_read_data(fid, coeffs, paulis) < 0) {
+		ret = -DATA2_EHAMIL;
 		goto err_hamil_read;
-	paustr = malloc(sizeof *paustr * num_qubits);
-	if (!paustr)
+	}
+	unsigned char *paustr = malloc(sizeof *paustr * num_qubits);
+	if (paustr == NULL) {
+		ret = -DATA2_ENOMEM;
 		goto err_paustr_alloc;
+	}
 
 	for (size_t i = 0; i < num_terms; i++) {
 		for (size_t j = 0; j < num_qubits; j++) {
@@ -376,11 +423,8 @@ data2_hamil_foreach(
 		if (rc != 0)
 			break;
 	}
-
 	free(paustr);
-	free(paulis);
-	free(coeffs);
-	return rc;
+	ret = rc;
 
 err_paustr_alloc:
 err_hamil_read:
@@ -389,7 +433,7 @@ err_paulis_alloc:
 	free(coeffs);
 err_coeffs_alloc:
 err_getnums:
-	return -1;
+	return ret;
 }
 
 static int
@@ -397,10 +441,10 @@ trotter_open(data2_id fid, hid_t *grpid)
 {
 	const hid_t id = H5Gopen2(fid, DATA2_TROTTER_STEPS, H5P_DEFAULT);
 	if (id == H5I_INVALID_HID)
-		return -1;
+		return -DATA2_ETROTT;
 
 	*grpid = id;
-	return 0;
+	return DATA2_OK;
 }
 
 static void
@@ -412,34 +456,41 @@ trotter_close(hid_t grpid)
 int
 data2_trotter_get_factor(data2_id fid, double *factor)
 {
+	int ret = DATA2_OK;
+
 	hid_t grpid;
-	if (trotter_open(fid, &grpid) < 0)
+	if (trotter_open(fid, &grpid) < 0) {
+		ret = -DATA2_ETROTT;
 		goto err_open;
+	}
 
 	const hid_t attr_norm_id =
 		H5Aopen(grpid, DATA2_TROTTER_STEPS_TIME_FACTOR, H5P_DEFAULT);
-	if (attr_norm_id == H5I_INVALID_HID)
+	if (attr_norm_id == H5I_INVALID_HID) {
+		ret = -DATA2_ETROTT;
 		goto err_attr_open;
+	}
 	double n;
-	if (H5Aread(attr_norm_id, H5T_NATIVE_DOUBLE, &n) < 0)
+	if (H5Aread(attr_norm_id, H5T_NATIVE_DOUBLE, &n) < 0) {
+		ret = -DATA2_ETROTT;
 		goto err_attr_read;
+	}
 	*factor = n;
-
-	H5Aclose(attr_norm_id);
-	trotter_close(grpid);
-	return 0;
 
 err_attr_read:
 	H5Aclose(attr_norm_id);
 err_attr_open:
 	trotter_close(grpid);
 err_open:
-	return -1;
+
+	return ret;
 }
 
 int
 data2_trotter_write_values(data2_id fid, double *values[2], size_t num_values)
 {
+	int ret = DATA2_OK;
+
 	hid_t grpid, dspace, dset;
 
 	double *val_cont = malloc(sizeof(double) * 2 * num_values);
@@ -450,9 +501,10 @@ data2_trotter_write_values(data2_id fid, double *values[2], size_t num_values)
 		val_cont[2 * i + 1] = values[1][i];
 	}
 
-	if (trotter_open(fid, &grpid) < 0)
+	if (trotter_open(fid, &grpid) < 0) {
+		ret = -DATA2_ETROTT;
 		goto err_open;
-
+	}
 	if ((dspace = H5Screate_simple(2, (hsize_t[]){ num_values, 2 },
 		     NULL)) == H5I_INVALID_HID) {
 		goto err_fspace;
@@ -460,17 +512,14 @@ data2_trotter_write_values(data2_id fid, double *values[2], size_t num_values)
 	if ((dset = H5Dcreate2(grpid, DATA2_TROTTER_STEPS_VALUES,
 		     H5T_IEEE_F64LE, dspace, H5P_DEFAULT, H5P_DEFAULT,
 		     H5P_DEFAULT)) == H5I_INVALID_HID) {
+		ret = -DATA2_ETROTT;
 		goto err_dset;
 	}
 	if (H5Dwrite(dset, H5T_NATIVE_DOUBLE, H5S_ALL, dspace, H5P_DEFAULT,
-		    val_cont) < 0)
+		    val_cont) < 0) {
+		ret = -DATA2_ETROTT;
 		goto err_dset_write;
-
-	H5Dclose(dset);
-	H5Sclose(dspace);
-	trotter_close(grpid);
-	free(val_cont);
-	return 0;
+	}
 
 err_dset_write:
 	H5Dclose(dset);
@@ -480,39 +529,42 @@ err_fspace:
 	trotter_close(grpid);
 err_open:
 	free(val_cont);
-	return -1;
+
+	return ret;
 }
 
 int
 data2_trotter_read_values_test(
 	data2_id fid, double *values[2], size_t num_values)
 {
+	int ret = DATA2_OK;
+
 	hid_t grpid;
 
 	double *val_cont = malloc(sizeof(double) * 2 * num_values);
 	if (val_cont == NULL)
-		return -1;
+		return -DATA2_ENOMEM;
 
-	if (trotter_open(fid, &grpid) < 0)
+	if (trotter_open(fid, &grpid) < 0) {
+		ret = -DATA2_ETROTT;
 		goto err_open;
-
-	hid_t dset = H5Dopen2(grpid, DATA2_TROTTER_STEPS_VALUES, H5P_DEFAULT);
-	if (dset == H5I_INVALID_HID)
+	}
+	const hid_t dset =
+		H5Dopen2(grpid, DATA2_TROTTER_STEPS_VALUES, H5P_DEFAULT);
+	if (dset == H5I_INVALID_HID) {
+		ret = -DATA2_ETROTT;
 		goto err_dset;
-
+	}
 	if (H5Dread(dset, H5T_NATIVE_DOUBLE, H5S_ALL, H5S_ALL, H5P_DEFAULT,
-		    val_cont) < 0)
+		    val_cont) < 0) {
+		ret = -DATA2_ETROTT;
 		goto err_read;
+	}
 
 	for (size_t i = 0; i < num_values; i++) {
 		values[0][i] = val_cont[2 * i];
 		values[1][i] = val_cont[2 * i + 1];
 	}
-
-	H5Dclose(dset);
-	trotter_close(grpid);
-	free(val_cont);
-	return 0;
 
 err_read:
 	H5Dclose(dset);
@@ -520,5 +572,5 @@ err_dset:
 	trotter_close(grpid);
 err_open:
 	free(val_cont);
-	return -1;
+	return ret;
 }
