f2348e37ecbd10c226674b722db528b45cdf5b1a
Marek Miller <mlm@math.ku.dk>
2024-04-15 10:47:04 +0200

    Update script trotter_validate.py

---
+++ scripts/trotter_validate.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+
+import argparse
+from math import tau
+
+import h5py
+import numpy as np
+import scipy
+
+
+def parse_arguments():
+    parser = argparse.ArgumentParser(
+        prog="trotter_validate",
+        description="",
+        epilog="Quantum-for-Life",
+    )
+    parser.add_argument("filename", type=str, help="input file")
+    return parser.parse_args()
+
+
+if __name__ == "__main__":
+    args = parse_arguments()
+
+    with h5py.File(args.filename, "r") as f:
+        values = np.array(
+            [complex(z[0], z[1]) for z in f["trotter_steps/values"]])
+        time_factor = f["trotter_steps"].attrs["time_factor"]
+        ph = f["pauli_hamil"]
+        norm = ph.attrs["normalization"]
+        offset = ph.attrs["offset"]
+
+    fft_size = len(values)
+    y_fft = np.fft.fft(values)
+    y_fft_abs = [abs(yf) for yf in y_fft]
+
+    peaks = scipy.signal.find_peaks(y_fft_abs)[0]
+    fft_freqs = np.fft.fftfreq(fft_size)
+    peaks_freqs = [fft_freqs[p] / (norm * time_factor / tau) for p in peaks]
+    peaks_freqs.sort()
+
+    print([p + offset for p in peaks_freqs])
