0ce7c79584db88122550b459d7243bf2eb05a373
Marek Miller <mlm@math.ku.dk>
2024-05-18 08:21:44 +0200

    Generate partial input files for qdrift (2)

--- scripts/qdrift_sample.py
+++ scripts/qdrift_sample.py
@@ -3,6 +3,7 @@
 import argparse
 import h5py
 import shutil
+import math
 
 
 def parse_arguments():
@@ -13,21 +14,31 @@ def parse_arguments():
     )
 
     parser.add_argument("filename", type=str, help="Input file")
-    parser.add_argument("--num-samples", type=int, default=128, help="number of QDIRFT samples")
-    parser.add_argument("--step-size", type=float)
-    parser.add_argument("--depth", type=int)
-    parser.add_argument("--outs", type=int)
+    parser.add_argument("--delta", type=float)
+    parser.add_argument("--epsilon", type=float)
+    parser.add_argument("--num-samples", type=int)
+
     return parser.parse_args()
 
 
 if __name__ == "__main__":
     args = parse_arguments()
 
-    for i in range(1, args.outs + 1):
+    with h5py.File(args.filename, "r") as f:
+        norm = f['pauli_hamil'].attrs['normalization']
+
+    delta = args.delta
+    epsilon = args.epsilon
+    J = math.ceil(math.log2(delta / (epsilon * norm)))
+    x = math.pow(2, -1 * J)
+    print(J)
+    print(x)
+
+    for i in range(0, J + 1):
         filename = args.filename + f"-{i:04}"
         shutil.copy(args.filename, filename)
         with h5py.File(filename, "a") as f:
             grp = f.create_group("trotter_steps")
-            grp.attrs["step_size"] = args.step_size
+            grp.attrs["step_size"] = x
             grp.attrs["num_samples"] = args.num_samples
-            grp.attrs["depth"] = args.depth
+            grp.attrs["depth"] = int(math.pow(2, i + J))
