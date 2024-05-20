ecde4261e305047fa16f7b29e7a5e59a6375d299
Marek Miller <mlm@math.ku.dk>
2024-05-20 12:18:34 +0200

    Add Robust phase estimation (4)

--- scripts/qdrift_rpe.py
+++ scripts/qdrift_rpe.py
@@ -3,6 +3,7 @@
 import argparse
 import h5py
 import math
+import cmath
 
 
 def parse_arguments():
@@ -27,7 +28,7 @@ if __name__ == "__main__":
         grp = f['pauli_hamil']
         norm = grp.attrs['normalization']
         offset = grp.attrs['offset']
-        pos_shift = grp.attrs['positive_shift']
+        # pos_shift = grp.attrs['positive_shift']
 
     delta = args.delta
     epsilon = args.epsilon
@@ -35,28 +36,30 @@ if __name__ == "__main__":
     x = math.pow(2, -1 * J)
 
     thetas = [0.0]
-    # zets = []
-    # phis = []
     for i in range(0, J + 1):
         filename = args.filename + f"-{i:04}"
         with h5py.File(filename, "a") as f:
             grp = f["trotter_steps"]
             depth = grp.attrs["depth"]
-            z_reim = grp["values"][0]
-            z = z_reim[0] + 1j * z_reim[1]
-            phi = math.acos((z / abs(z)).real)
-            S = [2 ** (-i) * (2 * math.pi * k + phi) for k in range(0, 2 ** i)]
-            # print(S)
-            D = [abs(thetas[i] - S[k]) for k in range(0, 2 ** i)]
-            # print(D)
-            k = 0
-            while k < 2 ** i:
-                if abs(thetas[i] - S[k]) == min(D):
-                    break
-                k += 1
-            thetas.append(S[k])
-
-    E0 = (math.sqrt(1 - x * x)) / (norm * x) * math.tan(x * thetas[J])
+            num_samples = grp.attrs["num_samples"]
+            z_re = sum(x[0] for x in grp["values"]) / num_samples
+            z_im = sum(x[1] for x in grp["values"]) / num_samples
+        z = z_re + 1j * z_im
+        phi = cmath.phase(z)
+        if phi < 0:
+            phi = 2 * math.pi + phi
+        S = [2 ** (-i) * (2 * math.pi * k + phi) for k in range(0, 2 ** i)]
+        D = [abs(thetas[i] - S[k]) for k in range(0, 2 ** i)]
+        k = 0
+        while k < 2 ** i:
+            if abs(thetas[i] - S[k]) == min(D):
+                break
+            k += 1
+        thetas.append(S[k])
+    thJ = thetas[J]
+    if thJ > math.pi:
+        thJ = - (2 * math.pi - thJ)
+    E0 = (math.sqrt(1 - x * x)) / (norm * x) * math.tan(x * thJ)
     print(E0)
-    print(E0 - pos_shift)
+    # print(E0 - pos_shift)
     print(E0 + offset)
