6aa9130fb2b522f2d5ab20318f4ed385609d894b
⧉⧉⧉ <marek.miller@protonmail.com>
2024-06-13 14:36:08 +0200

    Put missing declaration in circ_qdrift.h

--- src/circ_qdrift.h
+++ src/circ_qdrift.h
@@ -37,6 +37,9 @@ void
 circ_qdrift_data_destroy(struct circ_qdrift_data *cd);
 
 int
+circ_data_from_file(struct circ_qdrift_data *cd, data_id fid);
+
+int
 circ_qdrift_simulate(const struct circ_qdrift_data *cd);
 
 #endif // PHASE2_CIRC_QDRIFT_H
