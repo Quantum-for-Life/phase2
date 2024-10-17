14005413b55f67ba36e8387be963a6e06fd38f7e
⧉⧉⧉ <marek.miller@protonmail.com>
2024-10-17 00:04:17 +0000

    Move pauliEffect to a separate device function

--- phase2/qreg_cuda_lo.cu
+++ phase2/qreg_cuda_lo.cu
@@ -9,10 +9,11 @@
 #include "qreg_cuda.h"
 
 constexpr size_t threadPerBlock = 512;
+
 constexpr cuDoubleComplex half = { .x = 0.5, .y = 0.0 };
 
-__global__ void kernelMix(
-	cuDoubleComplex *a, cuDoubleComplex *b, cuDoubleComplex bm, size_t n)
+__global__ void kernelMix(cuDoubleComplex *__restrict__ a,
+	cuDoubleComplex *__restrict__ b, cuDoubleComplex bm, size_t n)
 {
 	size_t i = threadIdx.x + blockIdx.x * blockDim.x;
 	if (i >= n)
@@ -26,6 +27,32 @@ __global__ void kernelMix(
 	b[i] = cuCmul(z2, half);
 }
 
+// Assume z != nullptr
+__device__ uint64_t paulisEffect(
+	const struct paulis code, const uint64_t i, cuDoubleComplex *z)
+{
+	int mi = __popcll(i & code.pak[1]); // no. of minuses
+	int is = __popcll(code.pak[0] & code.pak[1]); // no. of i's
+	int r4 = (is + 2 * mi) & 0x3; // 4th root of unity
+	switch (r4) {
+	case 0:
+		break;
+	case 1:
+		*z = cuCmul(*z, (cuDoubleComplex){ .x = 0.0, .y = 1.0 });
+		break;
+	case 2:
+		*z = cuCmul(*z, (cuDoubleComplex){ .x = -1.0, .y = 0.0 });
+		break;
+	case 3:
+		*z = cuCmul(*z, (cuDoubleComplex){ .x = 0.0, .y = -1.0 });
+		break;
+	default:
+		unreachable();
+	}
+
+	return i ^ code.pak[0];
+}
+
 __global__ void kernelPauliRot(
 	cuDoubleComplex *a, size_t n, struct paulis code, double c, double s)
 {
@@ -33,33 +60,10 @@ __global__ void kernelPauliRot(
 	if (i >= n)
 		return;
 
-	size_t j = i ^ code.pak[0];
+	cuDoubleComplex z = { .x = 1.0, .y = 0.0 };
+	const uint64_t j = paulisEffect(code, i, &z);
 	if (j < i)
 		return;
-
-	cuDoubleComplex z = { .x = 1.0, .y = 0.0 };
-	{
-		int mi = __popcll(i & code.pak[1]); // no. of minuses
-		int is = __popcll(code.pak[0] & code.pak[1]); // no. of i's
-		int r4 = (is + 2 * mi) & 0x3; // 4th root of unity
-		switch (r4) {
-		case 0:
-			break;
-		case 1:
-			z.x = 0.0;
-			z.y = 1.0;
-			break;
-		case 2:
-			z.x = -1.0;
-			break;
-		case 3:
-			z.x = 0.0;
-			z.y = -1.0;
-			break;
-		default:
-			unreachable();
-		}
-	}
 
 	const cuDoubleComplex zi = a[i];
 	const cuDoubleComplex zj = a[j];
@@ -70,7 +74,8 @@ __global__ void kernelPauliRot(
 	a[j] = cuCadd(cuCmul(rc, zj), cuCmul(is, cuCmul(z, zi)));
 }
 
-__global__ void kernelAdd(cuDoubleComplex *a, cuDoubleComplex *b, size_t n)
+__global__ void kernelAdd(cuDoubleComplex *__restrict__ a,
+	cuDoubleComplex *__restrict__ b, size_t n)
 {
 	size_t i = threadIdx.x + blockIdx.x * blockDim.x;
 	if (i >= n)
@@ -88,7 +93,6 @@ void qreg_paulirot_lo(struct qreg *reg, const struct paulis *codes_lo,
 	const cuDoubleComplex z = { .x = creal(bm), .y = cimag(bm) };
 	kernelMix<<<blocks, threadPerBlock>>>(cu->damp, cu->dbuf, z, reg->namp);
 
-	// cudaDeviceSynchronize();
 	for (size_t k = 0; k < ncodes; k++) {
 		double c = cos(angles[k]), s = sin(angles[k]);
 		kernelPauliRot<<<blocks, threadPerBlock>>>(
@@ -97,7 +101,7 @@ void qreg_paulirot_lo(struct qreg *reg, const struct paulis *codes_lo,
 			cu->dbuf, reg->namp, codes_lo[k], c, -s);
 	}
 
-	// We mix again d_amp and d_buf. Sync them first.
+	// We mix again damp and dbuf. Sync them first.
 	cudaDeviceSynchronize();
 	kernelAdd<<<blocks, threadPerBlock>>>(cu->damp, cu->dbuf, reg->namp);
 }
